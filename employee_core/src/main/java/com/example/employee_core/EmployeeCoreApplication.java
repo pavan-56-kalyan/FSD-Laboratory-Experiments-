a28959b18b37e6321dd4308f5429351bcaecfd87
pavan-56-kalyan <vtu24943@veltech.edu.in>
2026-03-26 21:11:42 +0530

    Add files via upload

---
+++ employee_core/src/main/java/com/example/employee_core/EmployeeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCoreApplication.class, args);
+	}
+
+}
